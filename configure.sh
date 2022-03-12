6e04d8e87dbf9d4983b29412bfc9323cb1df6eb6
fbotp <46618620+fbotp@users.noreply.github.com>
2022-03-12 17:21:25 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -28,7 +28,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                 "disableInsecureEncryption": true
             },
             "streamSettings": {
-                "network": "ws"
+                "network": "tcp"
             }
         }
     ],
