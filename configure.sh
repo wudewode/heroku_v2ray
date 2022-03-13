70b668bb208efeacbcd7aef7711f4dc2dc41a084
fbotp <46618620+fbotp@users.noreply.github.com>
2022-03-13 18:00:54 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -30,7 +30,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
             "streamSettings": {
                 "network": "ws",
                 "wsSettings": {
-                    "path": "${WSPATH}"
+                    "path": "/"
                 }
             }
         }
